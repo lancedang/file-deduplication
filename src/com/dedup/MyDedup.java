ba0b775107e1dae4df374a7df2cea77b14672886
Leonard Mok <longyatmok@gmail.com>
2014-12-11 12:27:21 +0800

    RFP undone, max size chunk error need fix

--- src/com/dedup/MyDedup.java
+++ src/com/dedup/MyDedup.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.dedup;
 
@@ -32,288 +32,330 @@ import com.microsoft.windowsazure.services.core.storage.StorageException;
  */
 public class MyDedup {
 
-	/**
-	 * Parameter Bags for Cli request
-	 * 
-	 * @author NTF
-	 */
-	public static class RequestParameters {
-		public static enum RequestAction {
-			UPLOAD, DOWNLOAD, DELETE;
-		};
+    /**
+     * Parameter Bags for Cli request
+     *
+     * @author NTF
+     */
+    public static class RequestParameters {
 
-		public RequestAction action;
+        public static enum RequestAction {
 
-		/**
-		 * the window size (in bytes)
-		 */
-		public int m;
-		/**
-		 * the modulo parameter
-		 */
-		public int q;
-		/**
-		 * the maximum chunk size (in bytes)
-		 */
-		public int x;
-		/**
-		 * the base parameter
-		 */
-		public int d;
-		/**
-		 * the matching value that determines if a rabin fingerprint is an
-		 * anchor point
-		 */
-		public int v;
+            UPLOAD, DOWNLOAD, DELETE;
+        };
 
-		public StorageType storage;
+        public RequestAction action;
 
-		public String pathName;
+        /**
+         * the window size (in bytes)
+         */
+        public int m;
+        /**
+         * the modulo parameter
+         */
+        public int q;
+        /**
+         * the maximum chunk size (in bytes)
+         */
+        public int x;
+        /**
+         * the base parameter
+         */
+        public int d;
+        /**
+         * the matching value that determines if a rabin fingerprint is an
+         * anchor point
+         */
+        public int v;
 
-		public RequestParameters(RequestAction action, int m, int q, int x,
-				int d, int v, String pathName, StorageType type) {
-			this.action = action;
-			this.m = m;
-			this.q = q;
-			this.x = x;
-			this.d = d;
-			this.v = v;
-			this.pathName = pathName;
-			this.storage = type;
-		}
+        public StorageType storage;
 
-		public RequestParameters(RequestAction action, String pathName) {
-			this.action = action;
-			this.pathName = pathName;
-		}
-	}
+        public String pathName;
 
-	public static final String storageConnectionString = "DefaultEndpointsProtocol=https;"
-			+ "AccountName=portalvhdsnjtkkbt6f62w1;"
-			+ "AccountKey=i7/FNq8qQU/yyRxfqWlAGg73AWKtBUiWDkMfBA8uIXvU5RVVQzr487ZSEpBUAMj/2JRWNatQEM6DDPTv3c5Rcw==";
+        public RequestParameters(RequestAction action, int m, int q, int x,
+                int d, int v, String pathName, StorageType type) {
+            this.action = action;
+            this.m = m;
+            this.q = q;
+            this.x = x;
+            this.d = d;
+            this.v = v;
+            this.pathName = pathName;
+            this.storage = type;
+        }
 
-	/**
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
+        public RequestParameters(RequestAction action, String pathName) {
+            this.action = action;
+            this.pathName = pathName;
+        }
+    }
 
-		RequestParameters request = MyDedup.handleArgs(args);
+    public static final String storageConnectionString = "DefaultEndpointsProtocol=https;"
+            + "AccountName=portalvhdsnjtkkbt6f62w1;"
+            + "AccountKey=i7/FNq8qQU/yyRxfqWlAGg73AWKtBUiWDkMfBA8uIXvU5RVVQzr487ZSEpBUAMj/2JRWNatQEM6DDPTv3c5Rcw==";
 
-		if (request == null) {
-			/*
-			 * testcase1 upload 1024 4096 2048 64 1 testcases/rfp_anchor_mac
-			 * local
-			 */
-			System.out
-					.println("MyDedup - by @ntf, @longyakmok , @kousin197804");
-			System.out.println();
-			System.out
-					.println("upload <m> <q> <x> <d> <v> <file_to_upload> <local|remote>");
-			System.out.println("    m: the window size (in bytes)");
-			System.out.println("    q: the modulo parameter");
-			System.out.println("    x: the maximum chunk size (in bytes)");
-			System.out.println("    d: the base parameter");
-			System.out
-					.println("    v: the matching value that determines if a rabin fingerprint is an anchor point");
-			System.out.println("download <file_to_download>");
-			System.out.println("delete <file_to_delete>");
-			return;
-		}
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
 
-		try {
+        RequestParameters request = MyDedup.handleArgs(args);
 
-			StorageFactory.createStorage(StorageFactory.StorageType.LOCAL,
-					"data/");
-			StorageFactory.createStorage(StorageFactory.StorageType.AZURE,
-					storageConnectionString);
-			IStorage storage = StorageFactory.getStorage(request.storage);
+        if (request == null) {
+            /*
+             * testcase1 upload 1024 4096 2048 64 1 testcases/rfp_anchor_mac
+             * local
+             */
+            System.out
+                    .println("MyDedup - by @ntf, @longyakmok , @kousin197804");
+            System.out.println();
+            System.out
+                    .println("upload <m> <q> <x> <d> <v> <file_to_upload> <local|remote>");
+            System.out.println("    m: the window size (in bytes)");
+            System.out.println("    q: the modulo parameter");
+            System.out.println("    x: the maximum chunk size (in bytes)");
+            System.out.println("    d: the base parameter");
+            System.out
+                    .println("    v: the matching value that determines if a rabin fingerprint is an anchor point");
+            System.out.println("download <file_to_download>");
+            System.out.println("delete <file_to_delete>");
+            return;
+        }
 
-			// open or create index
-			Index index = MyDedup.indexExists() ? MyDedup.open() : new Index();
+        try {
 
-			switch (request.action) {
-			case DELETE:
-				deleteAction(request, System.out, index);
-				break;
-			case DOWNLOAD:
-				downloadAction(request, System.out, index);
-				break;
-			case UPLOAD:
-				uploadAction(request, System.out, index, storage);
-				break;
-			default:
-				break;
-			}
+            StorageFactory.createStorage(StorageFactory.StorageType.LOCAL,
+                    "data/");
+            StorageFactory.createStorage(StorageFactory.StorageType.AZURE,
+                    storageConnectionString);
+            IStorage storage = StorageFactory.getStorage(request.storage);
 
-			// save index
-			MyDedup.save(index);
+            // open or create index
+            Index index = MyDedup.indexExists() ? MyDedup.open() : new Index();
 
-			System.out.println("[DEBUG] =========end=========");
+            switch (request.action) {
+                case DELETE:
+                    deleteAction(request, System.out, index);
+                    break;
+                case DOWNLOAD:
+                    downloadAction(request, System.out, index);
+                    break;
+                case UPLOAD:
+                    uploadAction(request, System.out, index, storage);
+                    break;
+                default:
+                    break;
+            }
 
-		} catch (InvalidKeyException | URISyntaxException | StorageException
-				| IOException | ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+            // save index
+            MyDedup.save(index);
 
-	public static void uploadAction(RequestParameters request, PrintStream out,
-			Index index, IStorage storage) throws IOException {
-		File file = new File(request.pathName);
-		if (!file.exists()) {
-			throw new FileNotFoundException("input file not found.");
-		}
-		FileReader in = new FileReader(request.pathName);
+            System.out.println("[DEBUG] =========end=========");
 
-		int b;
-		ArrayList<Integer> offsets = new ArrayList<Integer>();
+        } catch (InvalidKeyException | URISyntaxException | StorageException | IOException | ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-		offsets.add(0);
+    public static void uploadAction(RequestParameters request, PrintStream out,
+            Index index, IStorage storage) throws IOException {
+        File file = new File(request.pathName);
+        if (!file.exists()) {
+            throw new FileNotFoundException("input file not found.");
+        }
+        FileInputStream in = new FileInputStream(request.pathName);
 
-		int size = 0;
-		int rfp = 0;
-		// @see tut9_assg3-rfp.pdf p.31
+        int b;
+        ArrayList<Integer> offsets = new ArrayList<Integer>();
+        offsets.add(0);
+        int size = 0;
+        int lastRfp = 0;
+        int offset = 0;
+        ArrayList<Integer> byteStore = new ArrayList<Integer>();
 
-		// Rabin Fingerprint here
-		while ((b = in.read()) != -1) {
+        // @see tut9_assg3-rfp.pdf p.31
+        // Rabin Fingerprint here
+        while ((b = in.read()) != -1) {
+            int rfp = 0;
+            int data = (int) (b & 0xFF);
+            byteStore.add(data);
+            offset++;
+            size++;
 
-			// we hit the maximum chunk size , we must put anchor point here
-			if (size == request.x) {
-				// TODO
-			}
+            // if the size reach max chunk size
+            if (size == request.x) {
+                offsets.add(offset);
+                size = 0;
+            }
 
-			// this is our interested value, put anchor point here
-			if (rfp == request.v) {
-				// TODO
-			}
-			size++;
-			// we read one byte b
-			// calculate the RFP
+            // calculate RFP
+            if (byteStore.size() >= request.m) {
+                if (byteStore.size() == request.m) {
+                    for (int i = 1; i <= request.m; i++) {
+                        int t = byteStore.get(i-1);
+                        int e = request.m - i;
+                        int mod = (int) modExpOpt(request.d, e, request.q);
+                        rfp += (t * mod) % request.q;
+                    }
+                    rfp = rfp % request.q;
+                } else if (byteStore.size() == request.m + 1) {
+                    int lastByte = byteStore.remove(0);
+                    int mod = (int)modExpOpt(request.d, request.m-1, request.q);
+                    rfp = ((((request.d % request.q) * ((lastRfp % request.q) - (((mod % request.q) * (lastByte % request.q)) % request.q) % request.q)) % request.q) + (data % request.q)) % request.q;
+                            //request.d * (lastRfp % request.q) - ((int) modExpOpt(request.d, request.m - 1, request.q) * (lastByte % request.q));
+                    if(rfp < 0){
+                        rfp += request.q;
+                    }
+                }
+                System.out.println("ps:"+ rfp +"\toffset:"+offset+"\tsize:"+size+"\tlastRfp:"+lastRfp);
+            }
 
-		}
+            if(rfp == request.v){
+                offsets.add(offset);
+                size = 0;
+            }
+            
+            lastRfp = rfp;
+        }
+        // size = file size here
+        // DEBUG
+        for(int i : offsets){
+            System.out.println(i);
+        }
+        in.close();
+    }
 
-		// size = file size here
-		in.close();
-	}
+    public static void downloadAction(RequestParameters request,
+            PrintStream out, Index index) throws InvalidKeyException,
+            URISyntaxException, StorageException, IOException {
 
-	public static void downloadAction(RequestParameters request,
-			PrintStream out, Index index) throws InvalidKeyException,
-			URISyntaxException, StorageException, IOException {
+        if (!index.hasFile(request.pathName)) {
+            throw new FileNotFoundException("File not found");
+        }
+        FileOutputStream output = new FileOutputStream(new File(
+                request.pathName));
 
-		if (!index.hasFile(request.pathName)) {
-			throw new FileNotFoundException("File not found");
-		}
-		FileOutputStream output = new FileOutputStream(new File(
-				request.pathName));
+        // SHA-1 String , Chunk pair
+        for (Entry<String, Chunk> pair : index.getChunks(request.pathName)) {
+            IStorage storage = StorageFactory.getStorage(pair.getValue().type);
+            Chunk chunkData = storage.get(pair.getKey());
+            output.write(chunkData.data);
+        }
+        output.close();
+    }
 
-		// SHA-1 String , Chunk pair
-		for (Entry<String, Chunk> pair : index.getChunks(request.pathName)) {
-			IStorage storage = StorageFactory.getStorage(pair.getValue().type);
-			Chunk chunkData = storage.get(pair.getKey());
-			output.write(chunkData.data);
-		}
-		output.close();
-	}
+    public static void deleteAction(RequestParameters request, PrintStream out,
+            Index index) throws FileNotFoundException, InvalidKeyException,
+            URISyntaxException, StorageException {
+        ArrayList<Map.Entry<String, Chunk>> deleteList = new ArrayList<Map.Entry<String, Chunk>>();
+        if (!index.hasFile(request.pathName)) {
+            throw new FileNotFoundException("File not found");
+        }
 
-	public static void deleteAction(RequestParameters request, PrintStream out,
-			Index index) throws FileNotFoundException, InvalidKeyException,
-			URISyntaxException, StorageException {
-		ArrayList<Map.Entry<String, Chunk>> deleteList = new ArrayList<Map.Entry<String, Chunk>>();
-		if (!index.hasFile(request.pathName)) {
-			throw new FileNotFoundException("File not found");
-		}
+        for (Entry<String, Chunk> pair : index.getUniqueChunks(request.pathName).entrySet()) {
+            pair.getValue().decrement();
+            if (pair.getValue().shouldDelete()) {
+                deleteList.add(pair);
+            } else {
+                index.chunks.put(pair.getKey(), pair.getValue());
+            }
+        }
 
-		for (Entry<String, Chunk> pair : index.getUniqueChunks(request.pathName).entrySet()) {
-			pair.getValue().decrement();
-			if (pair.getValue().shouldDelete()) {
-				deleteList.add(pair);
-			} else {
-				index.chunks.put(pair.getKey(), pair.getValue());
-			}
-		}
+        for (Entry<String, Chunk> pair : deleteList) {
+            out.println("[DEBUG] " + pair.getKey() + " removed from "
+                    + pair.getValue().type.toString());
+            IStorage storage = StorageFactory.getStorage(pair.getValue().type);
+            storage.remove(pair.getKey());
+            index.chunks.remove(pair.getKey());
+        }
+        index.files.remove(request.pathName);
+        out.println("Delete done and " + deleteList.size()
+                + " chucks are removed");
+    }
 
-		for (Entry<String, Chunk> pair : deleteList) {
-			out.println("[DEBUG] " + pair.getKey() + " removed from "
-					+ pair.getValue().type.toString());
-			IStorage storage = StorageFactory.getStorage(pair.getValue().type);
-			storage.remove(pair.getKey());
-			index.chunks.remove(pair.getKey());
-		}
-		index.files.remove(request.pathName);
-		out.println("Delete done and " + deleteList.size()
-				+ " chucks are removed");
-	}
+    public static String calculateFingerprint(byte[] data) {
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("SHA-1");
+            md.update(data);
+            byte[] checksum = md.digest();
+            return new BigInteger(1, checksum).toString(16);
+        } catch (NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-	public static String calculateFingerprint(byte[] data) {
-		MessageDigest md;
-		try {
-			md = MessageDigest.getInstance("SHA-1");
-			md.update(data);
-			byte[] checksum = md.digest();
-			return new BigInteger(1, checksum).toString(16);
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        return null;
+    }
 
-		return null;
-	}
+    public static RequestParameters handleArgs(String[] args) {
+        if (args.length < 1) {
+            return null;
+        }
 
-	public static RequestParameters handleArgs(String[] args) {
-		if (args.length < 1) {
-			return null;
-		}
+        switch (args[0]) {
+            case "upload":
+                if (args.length != 8) {
+                    return null;
+                }
+                StorageFactory.StorageType type = args[7].equals("local") ? StorageFactory.StorageType.LOCAL
+                        : StorageFactory.StorageType.AZURE;
+                return new MyDedup.RequestParameters(
+                        MyDedup.RequestParameters.RequestAction.UPLOAD,
+                        Integer.parseInt(args[1]), Integer.parseInt(args[2]),
+                        Integer.parseInt(args[3]), Integer.parseInt(args[4]),
+                        Integer.parseInt(args[5]), args[6], type);
+            case "download":
+                if (args.length != 2) {
+                    return null;
+                }
+                return new MyDedup.RequestParameters(
+                        MyDedup.RequestParameters.RequestAction.DOWNLOAD, args[1]);
+            case "delete":
+                if (args.length != 2) {
+                    return null;
+                }
+                return new MyDedup.RequestParameters(
+                        MyDedup.RequestParameters.RequestAction.DELETE, args[1]);
+        }
+        return null;
+    }
 
-		switch (args[0]) {
-		case "upload":
-			if (args.length != 8) {
-				return null;
-			}
-			StorageFactory.StorageType type = args[7].equals("local") ? StorageFactory.StorageType.LOCAL
-					: StorageFactory.StorageType.AZURE;
-			return new MyDedup.RequestParameters(
-					MyDedup.RequestParameters.RequestAction.UPLOAD,
-					Integer.parseInt(args[1]), Integer.parseInt(args[2]),
-					Integer.parseInt(args[3]), Integer.parseInt(args[4]),
-					Integer.parseInt(args[5]), args[6], type);
-		case "download":
-			if (args.length != 2) {
-				return null;
-			}
-			return new MyDedup.RequestParameters(
-					MyDedup.RequestParameters.RequestAction.DOWNLOAD, args[1]);
-		case "delete":
-			if (args.length != 2) {
-				return null;
-			}
-			return new MyDedup.RequestParameters(
-					MyDedup.RequestParameters.RequestAction.DELETE, args[1]);
-		}
-		return null;
-	}
+    public static void save(Index index) throws IOException {
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(
+                "mydedup.meta"));
+        oos.writeObject(index);
+        oos.close();
+    }
 
-	public static void save(Index index) throws IOException {
-		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(
-				"mydedup.meta"));
-		oos.writeObject(index);
-		oos.close();
-	}
+    public static boolean indexExists() {
+        File file = new File("mydedup.meta");
+        return file.exists();
+    }
 
-	public static boolean indexExists() {
-		File file = new File("mydedup.meta");
-		return file.exists();
-	}
+    static public Index open() throws IOException, ClassNotFoundException {
 
-	static public Index open() throws IOException, ClassNotFoundException {
+        ObjectInputStream objectinputstream = new ObjectInputStream(
+                new FileInputStream("mydedup.meta"));
+        Index index = (Index) objectinputstream.readObject();
+        objectinputstream.close();
 
-		ObjectInputStream objectinputstream = new ObjectInputStream(
-				new FileInputStream("mydedup.meta"));
-		Index index = (Index) objectinputstream.readObject();
-		objectinputstream.close();
+        return index;
 
-		return index;
+    }
 
-	}
-
+    public static long modExpOpt(int d, int e, int q) {
+        // Compute d^e mod q
+        long result = 1;
+        while (e > 0) {
+            if ((e & 1) == 1) {
+                result = (result * d) % q;
+            }
+            e = (e - (e & 1)) >> 1;
+            d = (d * d) % q;
+        }
+        return result;
+    }
 }
