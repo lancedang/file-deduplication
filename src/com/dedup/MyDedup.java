ced3ca7b7dfe6d2f6186d8f80734f3d33c98516a
Ng Ting Fung <ntf123@gmail.com>
2014-12-13 20:26:53 +0800

    fix performance issue by BufferedInputStream

--- src/com/dedup/MyDedup.java
+++ src/com/dedup/MyDedup.java
@@ -8,21 +8,18 @@ import com.dedup.storage.StorageFactory;
 import com.dedup.storage.StorageFactory.StorageType;
 import com.microsoft.windowsazure.services.core.storage.StorageException;
 
-import java.io.BufferedReader;
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.PrintStream;
 import java.math.BigInteger;
 import java.net.URISyntaxException;
 import java.nio.ByteBuffer;
-import java.nio.MappedByteBuffer;
 import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -178,8 +175,9 @@ public class MyDedup {
 		if (!file.exists()) {
 			throw new FileNotFoundException("input file not found.");
 		}
-		FileInputStream in = new FileInputStream(request.pathName);
+		// FileInputStream in = new FileInputStream(request.pathName);
 
+		BufferedInputStream in = new BufferedInputStream(new FileInputStream(request.pathName));
 		int b;
 		ArrayList<Integer> offsets = new ArrayList<Integer>();
 		ArrayList<String> chunks = new ArrayList<String>();
